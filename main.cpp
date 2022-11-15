e584cb06b73c079b89c372e881219eda28eb60ee
losimen <koval.pavlo03@gmail.com>
2022-11-15 22:49:30 +0200

    add simulation

--- main.cpp
+++ main.cpp
@@ -71,19 +71,29 @@ void ssol()
         unsigned int arg2 = state.mem[it] & 0x7;
         unsigned int addressField = state.mem[it] & 0xFFF;
 
-        std::cout << num << "| "
-                  << "addr: " << lineCounter++
-                  << "| OP: " << opcode
-                  << "| ARG0: " << arg0
-                  << "| ARG1: " << arg1
-                  << "| ARg2: " << arg2
-                  << "| Adr: " << addressField
-                  << " [" << state.mem[it] << "] "
-                  << std::endl;
+//        std::cout << num << "| "
+//                  << "addr: " << lineCounter++
+//                  << "| OP: " << opcode
+//                  << "| ARG0: " << arg0
+//                  << "| ARG1: " << arg1
+//                  << "| ARG2: " << arg2
+//                  << "| Adr: " << addressField
+//                  << " [" << state.mem[it] << "] "
+//                  << std::endl;
 
-        if (opcodes[LW] == opcode)
+        if (opcodes[ADD] == opcode)
         {
-            state.registers[arg1] = state.mem[addressField];
+            std::cout << "ADD: " << arg2 << " = " << arg0 << " + " << arg1 << std::endl;
+            std::cout << "ADD: " << state.registers[arg2] << " = " << state.registers[arg0] << " + " << state.registers[arg1] << std::endl << std::endl;
+            state.registers[arg2] = state.registers[arg0] + state.registers[arg1];
+        }
+        else if (opcodes[LW] == opcode)
+        {
+            state.registers[arg1] = state.mem[state.registers[arg0] + addressField];
+        }
+        else if (opcodes[HALT] == opcode)
+        {
+            break;
         }
 
     }
@@ -98,7 +108,7 @@ void ssol()
 
 int main()
 {
-    // asol();
+    //asol();
     ssol();
 
     return 0;
