373ea88688b487d44616743287c623140b4ff297
losimen <koval.pavlo03@gmail.com>
2022-11-15 21:28:08 +0200

    added parser of machine code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <bitset>
+
 
 #include "fileworker.h"
 #include "syntaxanalyzer.h"
@@ -7,25 +9,51 @@
 
 int main()
 {
-    SyntaxAnalyzer syntaxAnalyzer;
-    CodeGenerator codeGenerator;
+//    SyntaxAnalyzer syntaxAnalyzer;
+//    CodeGenerator codeGenerator;
+//
+//    FileWorker fileWorkerIn("example.txt");
+//    FileWorker fileWorkerOut("output.txt");
+//
+//    FileContent fileContent = fileWorkerIn.read();
+//
+//    Commands commands;
+//    std::vector<std::string> labels;
+//
+//    try
+//    {
+//        commands = syntaxAnalyzer.analyzeCode(fileContent);
+//        codeGenerator.generate(fileWorkerOut, commands);
+//    }
+//    catch (std::exception &e)
+//    {
+//        std::cerr << e.what() << std::endl;
+//    }
 
-    FileWorker fileWorkerIn("example.txt");
-    FileWorker fileWorkerOut("output.txt");
+    FileWorker fileWorker("output.txt");
 
-    FileContent fileContent = fileWorkerIn.read();
+    FileContent fileContent = fileWorker.read();
 
-    Commands commands;
-    std::vector<std::string> labels;
-
-    try
+    unsigned int lineCounter = 0;
+    for (auto &line: fileContent)
     {
-        commands = syntaxAnalyzer.analyzeCode(fileContent);
-        codeGenerator.generate(fileWorkerOut, commands);
+        int machineCode = std::stoi(line);
+        std::bitset<24> num(machineCode);
+
+        unsigned int opcode = machineCode >> SHIFT_OP;
+        unsigned int arg0 = (machineCode >> SHIFT_ARG0) & 0x7;
+        unsigned int arg1 = (machineCode >> SHIFT_ARG1) & 0x7;
+        unsigned int arg2 = machineCode & 0x7;
+        unsigned int addressField = machineCode & 0xFFF;
+
+        std::cout << num << "| "
+                  << "line: " << ++lineCounter
+                  << "| OP: " << opcode
+                  << "| ARG0: " << arg0
+                  << "| ARG1: " << arg1
+                  << "| ARg2: " << arg2
+                  << "| Adr: " << addressField << std::endl;
     }
-    catch (std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+
     return 0;
 }
